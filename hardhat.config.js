9c948e2f6f27b330cdb532cc1975bc5613176171
iHux3 <ihux3x@gmail.com>
2021-10-26 11:19:44 +0200

    etherscan verification

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,6 +3,7 @@ require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-web3");
 require("hardhat-gas-reporter");
 require("hardhat-abi-exporter");
+require("@nomiclabs/hardhat-etherscan");
 
 module.exports = {
     solidity: {
@@ -25,10 +26,17 @@ module.exports = {
             accounts: [`0x${process.env.PRIVATE_KEY}`],
             gasPrice: 1e9
         },
+        mainnet: {
+            url: `https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
+            accounts: [`0x${process.env.PRIVATE_KEY}`],
+        },
     },
     abiExporter: {
         path: './abi',
         clear: true,
         flat: true,
+    },
+    etherscan: {
+        apiKey: process.env.ETHERSCAN_KEY
     }
 };
